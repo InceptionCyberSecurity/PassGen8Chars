2a6b6c9214d4658917d96de810828d02c58bf598
Arcadeus OPS <info@arcadeusops.com>
2023-03-21 17:09:17 +0800

    Minor edits

--- passgen8.py
+++ passgen8.py
@@ -10,7 +10,7 @@ from email import encoders
 time.sleep(8)
 print("   ")
 print(" Specify your Send/Recieve mails in LINES 28 and 29 BEFORE YOU BEGIN! ")
-print(" Terminate by hitting Ctrl+C or Ctrl+Z if you haven't done this. ")
+print(" Terminate by hitting Ctrl+C or Ctrl+Z if you haven't done this yet. ")
 print("   ")
 # define password set
 def generate_password(character_set):
